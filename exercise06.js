eefa7692be379044f864a26c26f711aee3606ba0
JonnathanI <jjinaguazo@sudamericano.edu.ec>
2023-07-27 17:00:32 -0500

    tarea semana 15 resuelta

--- exercise06.js
+++ exercise06.js
@@ -1,1 +1,17 @@
+let suma = 0;
+let result;
+let nums =[12,14,5,7];
+let comp =7;
 
+function sumMulti(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i]%comp==0) {
+            suma = suma + nums[i]; 
+        }
+        
+    }
+    return suma;
+}
+
+result = sumMulti(nums);
+console.log(result);
--- exercise07.js
+++ exercise07.js
@@ -1,1 +1,17 @@
+let suma = 0;
+let result;
+let nums =[2,1,4,3];
 
+
+function sumIndiEle(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i]==i) {
+            suma = suma + nums[i]; 
+        }
+        
+    }
+    return suma;
+}
+
+result = sumIndiEle(nums);
+console.log(result);
